5c1188504524bc8546d45a849418b531aff1d715
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-06-04 12:00:39 -0400

    add error button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,9 @@ defineProps({
   msg: String,
 })
 
+const throwError = () => {
+  throw new Error("ERROR!")
+}
 const count = ref(0)
 </script>
 
@@ -18,7 +21,7 @@ const count = ref(0)
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-
+  <button type="button" @click="throwError">Throw Error</button>
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
