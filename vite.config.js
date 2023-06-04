c85f3b641a09c9032d4a333b6ff5d0d9ce344a17
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-06-04 12:46:12 -0400

    set up vite plugin and build start commands

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+
+
+export default ({}) => {
+  process.env = {...process.env, ...loadEnv( process.cwd(), "")};
+
+  return defineConfig({
+  
+  build: {
+    sourcemap: true,
+    outDir: 'distVite',
+  },
+  plugins: [vue(),
+    sentryVitePlugin({
+      org: process.env.VITE_SENTRY_ORG,
+      project: process.env.VITE_SENTRY_PROJECT,
+      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
+      release: {name: "vite1"},
+      sourcemaps: {assets: "distVite/**"}
+      
+    })
+  
+  ],
 })
+}
